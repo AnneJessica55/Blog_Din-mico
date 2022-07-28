d6d41aec55ac5f14d8aad4ebc83995837538f6ee
AnneJessica55 <annejessica55@gmail.com>
2022-07-28 10:26:28 -0300

    Update 000
    This is the first update. I just developed the structure responsively

---
+++ _header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Icons made by <a href="https://www.freepik.com" title="Freepik"> Freepik </a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com-->
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="img/favicon_io/favicon.ico" type="image/x-icon">
+    <title><?php echo $site_name ?> .:. <?php echo $page_title ?></title>
+</head>
+<body>
+    <a id="top"></a>
+    <header>
+        <a href="#" title="Página inicial"><img src="img/logo Konesans.png" alt="Logotipo de Konesans"></a>
+        <h1>Konesans <small>Conheimento Direcionado</small></h1>
+    </header>
+
+    <nav>
+        <a href="/" title="Página inicial">
+            <i class="fa-solid fa-house fa-fw"></i>
+            <span>Início</span>
+        </a>
+        <a href="/contacts" title="Faça contato">
+            <i class="fa-solid fa-comments fa-fw"></i>
+            <span>Contatos</span>
+        </a>
+        <a href="/about" title="Sobre...">
+            <i class="fa-solid fa-circle-info fa-fw"></i>
+            <span>Sobre</span>
+        </a>
+        <a href="/user" title="Logue-se...">
+            <i class="fa-solid fa-user fa-fw"></i>
+            <span>Entrar</span>
+        </a>
+    </nav>
+
+    <main>
